4612ee517e78f07b7552b92622039fb3fcf65a74
157ayzBYC <rifadzchen@gmail.com>
2025-03-14 13:49:24 +0700

    1 commit

---
+++ src/Mobil.java
@@ -0,0 +1,13 @@
+public class Mobil {
+    String merk;
+    String model;
+    String spekMesin;
+    int tahun;
+
+
+
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil();
+        System.out.println("mobil");
+    }
+}
